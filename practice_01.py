7e8efb8be6d9f7ffe097636866abf51ff8d7a6be
Cut Nurul A'la <107464764+CutNurul@users.noreply.github.com>
2023-06-01 17:00:03 +0900

    Tugas 1

---
+++ practice_01.py
@@ -0,0 +1,10 @@
+pengambilan_produk = 1020000
+jumlah_kendaraan = 6
+
+if pengambilan_produk >= 1000000:
+    if jumlah_kendaraan > 8:
+        print("Anda memenuhi syarat menjadi distributor di daerah ini.")
+
+
+    else:print("Anda tidak memenuhi syarat kendaraan.")
+else:print("Anda tidak memenuhi syarat pengambilan produk.")
